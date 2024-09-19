4c8ecdef056fe6a7174673f5fda87d3deedc0c14
yourusername <youremail@>
2024-09-19 17:14:57 -0600

    Code added based around lists

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        //Unlike Python, C-sharp must specify the TYPE of list it is.
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Add numbers to the numbers list. When you are finished write the number 0");
+
+        int newNumber = -1;
+
+        while (newNumber != 0)
+        {
+             Console.Write("Add number: ");
+             newNumber = int.Parse(Console.ReadLine());
+             numbers.Add(newNumber);
+
+        }
+        // Counting and listing the total number of items within the list.
+        Console.WriteLine($"The sum is: {numbers.Count}");
+
+        double average = Queryable.Average(numbers.AsQueryable());
+        Console.WriteLine($"The average is {average}");
+
+        double max = numbers.Max();
+        Console.WriteLine($"The largest number is: {max}");
+
     }
 }
